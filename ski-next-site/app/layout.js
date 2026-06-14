b1b0c82c2e3d7d368071383e6d41a713205b6715
jenardandchelsey <jenardandchelsey@gmail.com>
2026-06-13 23:20:29 -0500

    Add files via upload

---
+++ ski-next-site/app/layout.js
@@ -0,0 +1,14 @@
+import './globals.css';
+
+export const metadata = {
+  title: 'SKI | Swedish Kitchen Installers',
+  description: 'IKEA kitchen, closet, wardrobe, and home system installation in Kansas City.',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
