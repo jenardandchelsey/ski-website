13742b35548b4ae70f5c9e2e34c2f1b26795732f
jenardandchelsey <jenardandchelsey@gmail.com>
2026-06-20 00:18:36 -0500

    Update next.config.js

--- ski-next-site/next.config.js
+++ ski-next-site/next.config.js
@@ -1,3 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'ski-kitchens.com' }],
+        destination: 'https://swedishkitcheninstallers.com/:path*',
+        permanent: true,
+      },
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'www.ski-kitchens.com' }],
+        destination: 'https://swedishkitcheninstallers.com/:path*',
+        permanent: true,
+      },
+    ];
+  },
+};
 module.exports = nextConfig;
